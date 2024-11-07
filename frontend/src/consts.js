6b1e08e325b3c52ef4c4b8c883d943c857538db7
Karim <kimohamed20000@gmail.com>
2024-11-07 19:40:43 +0200

    project setup

---
+++ frontend/src/consts.js
@@ -0,0 +1,50 @@
+export const CATEGORIES = [
+  {
+    name: "Electronics",
+    link: "/category/electronics",
+  },
+  {
+    name: "Clothing",
+    link: "/category/clothing",
+  },
+  {
+    name: "Shoes",
+    link: "/category/shoes",
+  },
+  {
+    name: "Accessories",
+    link: "/category/accessories",
+  },
+];
+export const collectionCardsData = [
+  {
+    title: "New Arrivals",
+    description: "Check out the latest additions to our men's collection.",
+    imageUrl: "/images/latest-arrivals.jpeg",
+    link: "/men-products?category=new-arrivals",
+    buttonText: "Explore New Arrivals",
+  },
+  {
+    title: "Best Sellers",
+    description: "Discover the most popular items in our men's collection.",
+    imageUrl: "/images/best-sellers.jpeg",
+    link: "/men-products?category=best-sellers",
+    buttonText: "Shop Best Sellers",
+  },
+];
+export const collectionCardsDataFemale = [
+  {
+    title: "New Arrivals",
+    description: "Check out the latest additions to our women's collection.",
+    imageUrl: "/images/latest-arrivals-f.jpeg",
+    link: "/women-products?category=new-arrivals",
+    buttonText: "Explore New Arrivals",
+  },
+  {
+    title: "Best Sellers",
+    description: "Discover the most popular items in our women's collection.",
+    imageUrl: "/images/best-sellers-f.jpeg",
+    link: "/women-products?category=best-sellers",
+    buttonText: "Shop Best Sellers",
+  },
+];
